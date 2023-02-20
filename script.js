65a4fe282b6129b2537e06726dd0754a36ef1cd6
Michael Paukov <michael.paukov@gmail.com>
2023-02-20 10:00:35 +0100

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2D");
+  canvas.width = 1280;
+  canvas.height = 720;
+});
